3bd2bea8f05de22beca045c4e6d665d354243445
RAFAEL MANARIN <31132720+LordManarin@users.noreply.github.com>
2020-03-25 17:04:26 -0300

    Update index.php

--- UEC/index.php
+++ UEC/index.php
@@ -2,19 +2,21 @@
 <pre>
 <?php
 require_once "Fighter.php";
-require_once "Lutadore.php";
-
+require_once "Fight.php";
 //criando lutadores
 $lutador = array();
 $lutador[0] = new Fighter("Rafael", "Brasil", 24, 1.78,73,11,2,3);
-$lutador[1] = new Fighter("Jasohn", "USA", 30, 1.98,98,5,5,5);
+$lutador[1] = new Fighter("Jasohn", "USA", 30, 1.98,80,5,5,5);
 $lutador[2] = new Fighter("DeadCode", "Australia", 24, 1.80, 119, 11, 9, 10);
 
+//marcando luta
+$UEC01 = new Fight();
+$UEC01->markFight($lutador[0],$lutador[1]);
+$UEC01->startFight();
 
-//testando codigo
-$lutador[2]->status();
-$lutador[2]->loseFight();
-$lutador[2]->status();
+$lutador[0]->status();
+$lutador[1]->status();
+
 
 ?>
 </pre> 
