709f65c748c20149c5125d12119c4bbaea22439b
RAFAEL MANARIN <31132720+LordManarin@users.noreply.github.com>
2020-03-25 09:36:09 -0300

    Add files via upload

---
+++ ControleRemoto/index.php
@@ -0,0 +1,19 @@
+<h1> Controle Remoto </h1>
+<pre>
+<?php
+require_once "ControleRemoto.php";
+
+$controller1=new ControleRemoto();
+
+$controller1->ligar();
+$controller1->maisVolume();
+$controller1->maisVolume();
+$controller1->maisVolume();
+$controller1->maisVolume();
+$controller1->abrirMenu();
+$controller1->desligar();
+$controller1->abrirMenu();
+
+?>
+</pre> 
+
